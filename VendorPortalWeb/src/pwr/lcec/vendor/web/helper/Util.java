ae495c624a334f49a2c3eae42759b3a260194d12
hconstant004 <45364974+hconstant004@users.noreply.github.com>
2018-12-21 13:29:35 -0500

    Update Util.java

--- VendorPortalWeb/src/pwr/lcec/vendor/web/helper/Util.java
+++ VendorPortalWeb/src/pwr/lcec/vendor/web/helper/Util.java
@@ -1,15 +1,7 @@
 package pwr.lcec.vendor.web.helper;
 
 public class Util {
-	public static final String INITIAL_CONTEXT_FACTORY = "com.sun.jndi.ldap.LdapCtxFactory";
-	public static final String SECURITY_AUTHENTICATION = "Simple";
-	public static final String SECURITY_PRINCIPAL = "lcec\\svc_soa-ssapi";
-	public static final String SECURITY_CREDENTIALS = "EfYAN7pwPfLb*1rx!rTzY0IWcU9Fq0tA!up8mshOqo!9!LvB!R";
-	public static final String PROVIDER_URL = "ldap://ad:389";
-	public static final String REFERRAL = "follow";
-
-	public static final String DOMAIN = "lcec\\";
-
+	
 	public static final String IMG_EXT = ".png";
 
 	public static final String MSG_PKG = "pwr.lcec.vendor.web.resource.messages";
